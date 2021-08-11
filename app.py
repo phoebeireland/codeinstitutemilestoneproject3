f744d3cb4e5f723893ea893a3c883447d1f5e4c1
Phoebe Ireland <pireland2016@gmail.com>
2021-08-11 19:14:04 +0100

    add some of the imports for app.py page and fix the base.html page so a python server will work

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import datetime
+from os import path
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+
+app = Flask(__name__)
+app.config.update(
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+)
+
+
+# Static page routes
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/registercompany')
+def registercompany():
+    return render_template('registercompany.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+# Error Handling of 404 & 500
+@app.errorhandler(404)
+def response_404(exception):
+    return render_template('404.html', exception=exception)
+
+
+@app.errorhandler(500)
+def response_500(exception):
+    return render_template('500.html', exception=exception)
+
+
+if __name__ == "__main__":
+    app.run(
+        host = os.environ.get('IP', '127.0.0.1'),
+        port = os.environ.get('PORT', '5000'),
+        debug=True
+    )
